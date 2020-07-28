1e21f0a91184139487559c86b8597eb1849ad2c9
Bruno Reis <bruno.p.reis@gmail.com>
2020-07-27 22:35:39 -0300

    delete run button

--- test-client/src/RunTimeline/TestRuns/TestRun/TestRun.js
+++ test-client/src/RunTimeline/TestRuns/TestRun/TestRun.js
@@ -1,18 +1,22 @@
-import React, { useContext } from 'react'
+import React, { useContext, useCallback } from 'react'
 import { TestRunWrapper } from './TestRunWrapper'
 import { Title } from './Title'
 import { TestResults } from './TestResults'
 import { TestRunsReducerContext } from './_uses'
 
 export const TestRun = ({ runId }) => {
-  const { getTestRun } = useContext(TestRunsReducerContext)
+  const { getTestRun, deleteRun } = useContext(TestRunsReducerContext)
   const { rootDir, testPathPattern } = getTestRun(runId)
+  const deleteThisRun = useCallback(() => deleteRun(runId), [runId, deleteRun])
 
   return (
     <TestRunWrapper>
       <Title>
         <div>{rootDir}</div>
-        <div>{testPathPattern ? `Pattern: ${testPathPattern}` : null}</div>
+        <div>
+          {testPathPattern ? `Pattern: ${testPathPattern}` : null}
+          <button onClick={deleteThisRun}>x</button>
+        </div>
       </Title>
       <TestResults runId={runId} />
     </TestRunWrapper>
